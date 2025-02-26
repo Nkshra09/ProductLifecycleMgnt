470be9679bcc087a7e5290fd3fdb96ce047fe651
Shraddanayak <nayakshradda@gmail.com>
2025-02-26 21:44:07 +0530

    Update products.php

--- products.php
+++ products.php
@@ -106,16 +106,17 @@
               <table class="table table-hover tm-table-small tm-product-table">
                 <thead>
                   <tr>
-                   <!-- <th scope="col">&nbsp;</th>  -->
+                    <th scope="col">&nbsp;</th>  
                     <th scope="col">PRODUCT NAME</th>
                     <th scope="col">UNIT SOLD</th>
                     <th scope="col">IN STOCK</th>
 		    <th scope="col">Date Of MFG</th>
                     <th scope="col">EXPIRE DATE</th>
-                    <!-- <th scope="col">&nbsp;</th>  -->
+                    <th scope="col">&nbsp;</th>  
                   </tr>
                 </thead>
                 <tbody>
+		
  <?php
 	 include 'dashboard.php';
 	// Fetch product data
@@ -135,12 +136,24 @@
                     echo "<td>" . $row["DateOfExp"] . "</td>";
                     echo "<td><a href='#' class='tm-product-delete-link'><i class='far fa-trash-alt tm-product-delete-icon'></i></a></td>";
                     echo "</tr>";
+		   echo " </tbody>";
+                  echo "</table>";
+                   echo "</div>";
                   }
                 } else {
                   echo "<tr><td colspan='7'>No products found</td></tr>";
                 }
                 $conn->close();
                 ?> 
+             <a
+              href="add-product.html"
+              class="btn btn-primary btn-block text-uppercase mb-3">Add new product</a>
+            <button class="btn btn-primary btn-block text-uppercase">
+              Delete selected products
+            </button>
+          </div>
+        </div>
+			
         <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4 tm-block-col">
           <div class="tm-bg-primary-dark tm-block tm-block-product-categories">
             <h2 class="tm-block-title">Product Categories</h2>
