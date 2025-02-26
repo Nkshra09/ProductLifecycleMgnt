acde0f157b0fd3f5beac2f9495c0f0d376bd9a20
Shraddanayak <nayakshradda@gmail.com>
2025-02-26 22:13:15 +0530

    Update and rename add-product.html to add-product.php

--- add-product.php
+++ add-product.php
@@ -44,7 +44,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mx-auto h-100">
             <li class="nav-item">
-              <a class="nav-link" href="index.html">
+              <a class="nav-link" href="index.php">
                 <i class="fas fa-tachometer-alt"></i> Dashboard
                 <span class="sr-only">(current)</span>
               </a>
@@ -85,14 +85,14 @@
                 aria-expanded="false"
               >
                 <i class="fas fa-cog"></i>
-                <span> Settings <i class="fas fa-angle-down"></i> </span>
+                <span> Expiry Status <i class="fas fa-angle-down"></i> </span>
               </a>
             
             </li>
           </ul>
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link d-block" href="login.html">
+              <a class="nav-link d-block" href="login.php">
                 Admin, <b>Logout</b>
               </a>
             </li>
@@ -119,23 +119,13 @@
                     </label>
                     <input
                       id="name"
-                      name="name"
+                      name="ProductName"
                       type="text"
                       class="form-control validate"
                       required
                     />
                   </div>
-                  <div class="form-group mb-3">
-                    <label
-                      for="description"
-                      >Description</label
-                    >
-                    <textarea
-                      class="form-control validate"
-                      rows="3"
-                      required
-                    ></textarea>
-                  </div>
+                 
                   <div class="form-group mb-3">
                     <label
                       for="category"
@@ -151,6 +141,16 @@
                       <option value="3">Trending</option>
                     </select>
                   </div>
+			 <div class="form-group mb-3">
+                    <label for="dateofmfg">DateOfMFG</label >
+		    <input
+                            id="expire_date"
+                            name="DateOfMFG"
+                            type="date"
+                            class="form-control validate"
+                            data-large-mode="true"
+                          />
+                  </div>
                   <div class="row">
                       <div class="form-group mb-3 col-xs-12 col-sm-6">
                           <label
@@ -159,8 +159,8 @@
                           </label>
                           <input
                             id="expire_date"
-                            name="expire_date"
-                            type="text"
+                            name="DateOfExp"
+                            type="date"
                             class="form-control validate"
                             data-large-mode="true"
                           />
@@ -172,7 +172,7 @@
                           </label>
                           <input
                             id="stock"
-                            name="stock"
+                            name="Quantity"
                             type="text"
                             class="form-control validate"
                             required
@@ -182,12 +182,7 @@
                   
               </div>
               <div class="col-xl-6 col-lg-6 col-md-12 mx-auto mb-4">
-                <div class="tm-product-img-dummy mx-auto">
-                  <i
-                    class="fas fa-cloud-upload-alt tm-upload-icon"
-                    onclick="document.getElementById('fileInput').click();"
-                  ></i>
-                </div>
+                
                 <div class="custom-file mt-3 mb-3">
                   <input id="fileInput" type="file" style="display:none;" />
                   <input
@@ -223,10 +218,6 @@
     <!-- https://jqueryui.com/download/ -->
     <script src="..static/js/bootstrap.min.js"></script>
     <!-- https://getbootstrap.com/ -->
-    <script>
-      $(function() {
-        $("#expire_date").datepicker();
-      });
-    </script>
+    
   </body>
 </html>
