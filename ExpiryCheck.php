2c0e4b8d2e90d4654e6539bf7afa3b8a5992d7f0
Shraddanayak <nayakshradda@gmail.com>
2025-02-26 16:18:55 +0530

    Update ExpiryCheck.php

--- ExpiryCheck.php
+++ ExpiryCheck.php
@@ -13,6 +13,62 @@
     <link rel="stylesheet" href="/ProductLifecycleMgnt/templatemo-style.css">
     <link rel="stylesheet" href="/ProductLifecycleMgnt/expiry.css">
 </head>
+  <body id="reportsPage">
+  <nav class="navbar navbar-expand-xl">
+    <div class="container h-100">
+      <a class="navbar-brand" href="index.php">
+        <h1 class="tm-site-title mb-0">Product Admin</h1>
+      </a>
+      <button class="navbar-toggler ml-auto mr-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars tm-nav-icon"></i>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto h-100">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">
+              <i class="fas fa-tachometer-alt"></i> Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="far fa-file-alt"></i>
+              <span> Reports <i class="fas fa-angle-down"></i> </span>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Daily Report</a>
+              <a class="dropdown-item" href="#">Weekly Report</a>
+              <a class="dropdown-item" href="#">Yearly Report</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="products.php">
+              <i class="fas fa-shopping-cart"></i> Products
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-cog"></i>
+              <span> Settings <i class="fas fa-angle-down"></i> </span>
+            </a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link d-block" href="login.php">
+              Admin, <b>Logout</b>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-5">
+    <div class="row tm-content-row">
+      <div class="col-sm-12 col-md-12 col-lg-8 col-xl-8 tm-block-col">
+        <div class="tm-bg-primary
 <body>
   <div class="container">
     <h1>Product Expiry Checker</h1>
