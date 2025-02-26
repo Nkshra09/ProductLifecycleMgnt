a3cab7212ab3499347583ef01e0bd96b01e072ee
Shraddanayak <nayakshradda@gmail.com>
2025-02-26 20:01:24 +0530

    Update ExpiryCheck.php

--- ExpiryCheck.php
+++ ExpiryCheck.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Product Expiry Checker</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
     <!-- https://fonts.google.com/specimen/Roboto -->
     <link rel="stylesheet" href="/ProductLifecycleMgnt/fontawesome.min.css">
