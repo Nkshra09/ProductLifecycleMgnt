463d840cb33d1e69355854140d25712626df9b98
Shraddanayak <nayakshradda@gmail.com>
2025-03-06 22:08:45 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -11,6 +11,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$mysqli->select_db("ProductDB");
+// $mysqli->select_db("ProductDB");
 
 ?>
