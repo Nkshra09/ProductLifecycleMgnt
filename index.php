57aec995774c66552e7db6122f8da4c07b1c981a
Shraddanayak <nayakshradda@gmail.com>
2025-03-06 22:19:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -64,9 +64,8 @@
                         </li>
 
                         
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="ExpiryCheck.php" id="navbarDropdown" role="button" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">
+                        <li class="nav-item ">
+                            <a class="nav-link " href="ExpiryCheck.php" >
                                 <i class="fas fa-cog"></i>
                                 <span>
                                     Expiry Status <i class="fas fa-angle-down"></i>
