630b7e59c9c5f562ff7cec895acd63d29f441b5d
Shraddanayak <nayakshradda@gmail.com>
2025-03-06 22:52:30 +0530

    Update add-product.php

--- add-product.php
+++ add-product.php
@@ -114,7 +114,7 @@
               <div class="col-xl-6 col-lg-6 col-md-12">
 
 <!-- 		Here insertion code starts       -->
-                <form action="products.php" method="post" class="tm-edit-product-form">
+                <form action="add-product.php" method="post" class="tm-edit-product-form">
                   <div class="form-group mb-3">
                     <label
                       for="name"
