54f030dc0b4ca7525c541f46c8027ba041427eae
Shraddanayak <nayakshradda@gmail.com>
2025-06-29 16:06:23 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "ProductDB"; // Replace with your actual database name
+$dbname = "productdb"; // Replace with your actual database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,6 +11,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// $mysqli->select_db("ProductDB");
+// $mysqli->select_db("prodcutdb");
 
 ?>
